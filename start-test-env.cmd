6e57282555a2803dd46d08f84fc92dc6eea917a6
jackstar12 <j@bol.tz>
2026-05-26 21:57:12 +0200

    chore: top level regtest setup

--- start-test-env.cmd
+++ start-test-env.cmd
@@ -1,5 +1,5 @@
 @echo off
 setlocal
-set "SCRIPT_DIR=%~dp0submodules\NNark\regtest"
-set "OVERRIDE_ENV=%~dp0submodules\NNark\.env.regtest"
-wsl -e bash -c "cd \"$(wslpath '%SCRIPT_DIR%')\" && if [ -f \"$(wslpath '%OVERRIDE_ENV%')\" ]; then sed -i 's/\r$//' \"$(wslpath '%OVERRIDE_ENV%')\"; fi && sed 's/\r$//' start-env.sh | bash -s -- --clean"
+set "SCRIPT_DIR=%~dp0regtest"
+set "OVERRIDE_ENV=%~dp0.env.regtest"
+wsl -e bash -lc "cd \"$(wslpath '%SCRIPT_DIR%')\" && if [ -f \"$(wslpath '%OVERRIDE_ENV%')\" ]; then sed -i 's/\r$//' \"$(wslpath '%OVERRIDE_ENV%')\"; fi && bash ./start-env.sh --clean"
