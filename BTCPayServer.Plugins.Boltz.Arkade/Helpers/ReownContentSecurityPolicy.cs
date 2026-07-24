54d5e1ca897566aa6a6d29f483c1c0081d6cbf9d
jackstar12 <j@bol.tz>
2026-07-24 20:47:55 +0200

    chore: simplify reown sources list

--- BTCPayServer.Plugins.Boltz.Arkade/Helpers/ReownContentSecurityPolicy.cs
+++ BTCPayServer.Plugins.Boltz.Arkade/Helpers/ReownContentSecurityPolicy.cs
@@ -9,26 +9,12 @@ public static class ReownContentSecurityPolicy
         ("connect-src",
         [
             "'self'",
-            "https://rpc.walletconnect.com",
-            "https://rpc.walletconnect.org",
-            "https://relay.walletconnect.com",
-            "https://relay.walletconnect.org",
-            "wss://relay.walletconnect.com",
-            "wss://relay.walletconnect.org",
-            "https://pulse.walletconnect.com",
-            "https://pulse.walletconnect.org",
-            "https://api.web3modal.com",
             "https://api.web3modal.org",
-            "https://keys.walletconnect.com",
-            "https://keys.walletconnect.org",
-            "https://notify.walletconnect.com",
-            "https://notify.walletconnect.org",
-            "https://echo.walletconnect.com",
-            "https://echo.walletconnect.org",
-            "https://push.walletconnect.com",
-            "https://push.walletconnect.org",
+            "https://pulse.walletconnect.org",
+            "https://rpc.walletconnect.org",
+            "wss://relay.walletconnect.org",
+            "https://www.walletlink.org",
             "wss://www.walletlink.org",
-            "https://cca-lite.coinbase.com",
             "https://arb1.arbitrum.io",
             "https://ethereum.reth.rs",
             "https://polygon.drpc.org"
@@ -36,22 +22,15 @@ public static class ReownContentSecurityPolicy
         ("frame-src",
         [
             "'self'",
-            "https://verify.walletconnect.com",
-            "https://verify.walletconnect.org",
-            "https://secure.walletconnect.com",
-            "https://secure.walletconnect.org"
+            "https://verify.walletconnect.org"
         ]),
         ("img-src",
         [
-            "'self'", "data:", "blob:",
-            "https://walletconnect.org", "https://walletconnect.com",
-            "https://secure.walletconnect.com", "https://secure.walletconnect.org",
-            "https://tokens-data.1inch.io", "https://tokens.1inch.io",
-            "https://ipfs.io", "https://cdn.zerion.io"
+            "'self'", "data:", "blob:"
         ]),
         ("font-src",
         [
-            "'self'", "data:", "https://fonts.gstatic.com", "https://fonts.reown.com"
+            "'self'", "https://fonts.reown.com"
         ])
     ];
 
